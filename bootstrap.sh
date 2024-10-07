0015c685080380bf95526ca4b88601882d75b7a3
Alex A. Naanou <alex.nanou@gmail.com>
2024-10-07 08:28:39 +0300

    bootstrapping...
    Signed-off-by: Alex A. Naanou <alex.nanou@gmail.com>

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,3 +1,41 @@
 #!/usr/bin/bash
 
+# XXX run self in nohup
+# XXX TEST!
+
+# bootstrap...
+#QUIET=
+#DRY_RUN=
+ECHO_PREFIX="### "
+function @ (){
+	if [ -z $DRY_RUN ] ; then
+		! [ $QUIET ] \
+			&& echo -e "${ECHO_PREFIX}$@"
+		"$@"
+	else
+		echo -e $@
+	fi
+	return $?
+}
+
+# XXX test if running as root...
+
+apt update
+apt upgrade
+
+# XXX
+apt install \
+	git make wget
+
+#git clone git@github.com:flynx/proxmox-utils.git
+git clone https://github.com/flynx/proxmox-utils.git
+
+cd proxmox-utils
+
+source ./.pct-helpers
+
+
+# XXX create bootstrap gate...
+# XXX
+
 
