3b29199f9cff48a9e2710dd4752d29e69ee78728
Alex A. Naanou <alex.nanou@gmail.com>
2024-10-11 22:13:28 +0300

    oops, seem to have overcomplicated things a bit...
    Signed-off-by: Alex A. Naanou <alex.nanou@gmail.com>

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,48 +4,19 @@
 #PROXMOX_UTILS=git@github.com:flynx/proxmox-utils.git
 PROXMOX_UTILS=${PROXMOX_UTILS:-https://github.com/flynx/proxmox-utils.git}
 
+# XXX do we need to update the system here?
+apt update
+apt upgrade
 
-
-# XXX run self in nohup
-# 		..."$ make host" will likely break existing connections...
-# XXX TEST!
-
-# bootstrap...
-# XXX might be a better idea to bootstrap the bootstrap by dowloading
-# 		the .pct-helpers...
-#QUIET=
-#DRY_RUN=
-ECHO_PREFIX="### "
-function @ (){
-	if [ -z $DRY_RUN ] ; then
-		! [ $QUIET ] \
-			&& echo -e "${ECHO_PREFIX}$@"
-		"$@"
-	else
-		echo -e $@
-	fi
-	return $?
-}
-
-# XXX test if running as root...
-# XXX
-
-@ apt update
-@ apt upgrade
-
-# XXX
-@ apt install \
+# keep this to the minimum...
+apt install \
 	git make wget
 
-@ git clone $PROXMOX_UTILS 
+git clone $PROXMOX_UTILS 
 
-@ cd proxmox-utils
+cd proxmox-utils
 
-source ./.pct-helpers
-
-
-# XXX create bootstrap gate...
-# XXX
+make bootstrap
 
 
 # vim:set ts=4 sw=4 nowrap :
